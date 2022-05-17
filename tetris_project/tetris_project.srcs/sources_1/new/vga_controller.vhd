c4a83b5c7b04fe33a9d9e9160cff8a4de331df39
Nestor E. Orozco-Llamas <f005fnt@dartmouth.edu>
2022-05-17 16:44:38 -0400

    adding vhdl

---
+++ tetris_project/tetris_project.srcs/sources_1/new/vga_controller.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/17/2022 04:42:05 PM
+-- Design Name: 
+-- Module Name: vga_controller - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity vga_controller is
+--  Port ( );
+end vga_controller;
+
+architecture Behavioral of vga_controller is
+
+begin
+
+
+end Behavioral;
