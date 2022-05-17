0bd87b69aae4b45cc6f261839a1e90be6b83b145
Jason C. Rickenbacher <jason.c.rickenbacher.ug@dartmouth.edu>
2022-05-17 19:25:12 -0400

    Finished VGA_TEST. Controller works.

--- tetris_project/tetris_project.srcs/sources_1/new/vga_controller.vhd
+++ tetris_project/tetris_project.srcs/sources_1/new/vga_controller.vhd
@@ -1,14 +1,14 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: Dartmouth College
+-- Engineers: Nestor Orozco-Llamas and Jason Rickenbacher
 -- 
 -- Create Date: 05/17/2022 04:51:34 PM
 -- Design Name: 
 -- Module Name: vga_controller - Behavioral
--- Project Name: 
--- Target Devices: 
+-- Project Name: VGA Controller
+-- Target Devices: Basys 3 (Artix 7)
 -- Tool Versions: 
--- Description: 
+-- Description: A controller for VGA outputs
 -- 
 -- Dependencies: 
 -- 
@@ -17,8 +17,6 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -44,44 +42,51 @@ end vga_controller;
 
 architecture Behavioral of vga_controller is
 
-
-signal h_count, v_count : unsigned(9 downto 0) := "0000000000";
+------------SIGNAL DECLARATIONS------------
+signal h_count, v_count : unsigned(9 downto 0) := (others => '0');
 signal h_tc, v_tc, h_video_on, v_video_on: STD_LOGIC := '0';
+
 begin
 
-counter : process(pixel_clk)
+counters : process(pixel_clk)
 begin
 
 if rising_edge(pixel_clk) then
-    
-    h_count <= h_count + 1;
-    
+
     if h_tc ='1' then
-        v_count <= v_count + 1;
-        h_count <= "0000000000";
-    end if;  
-     
-    if v_tc ='1' then
-        v_count <= "0000000000";
+        if v_tc = '1' then
+            v_count <= (others => '0'); --reset v_count
+        else
+            v_count <= v_count + 1; --increment v_count
+        end if;
+        
+        h_count <= (others => '0'); --reset h_count
+    else
+        h_count <= h_count + 1; --increment h_count
     end if;  
 end if;
 
-end process counter;
+end process counters;
 
+--LOW during retrace, otherwise HIGH
 hsync <= '1' when ((h_count < 656) or (h_count > 751)) else '0';
-vsync <= '1' when ((h_count < 490) or (h_count > 491)) else '0';
+vsync <= '1' when ((v_count < 490) or (v_count > 491)) else '0';
 
-v_video_on <= '1' when (v_count < 480) else '0';
+--HIGH when on screen, LOW when off screen in given coordinate direction
 h_video_on <= '1' when (h_count < 640) else '0';
+v_video_on <= '1' when (v_count < 480) else '0';
 
-vblank <= not(v_video_on);
+--LOW when on screen, HIGH when off screen in given coordinate direction
 hblank <= not(h_video_on);
+vblank <= not(v_video_on);
 
+--HIGH when both coordinates are on screen, otherwise LOW
 video_on <= (v_video_on and h_video_on);
 
 pixel_x <= STD_LOGIC_VECTOR(h_count);
 pixel_y <= STD_LOGIC_VECTOR(v_count);
 
+--Terminal Counts
 h_tc <= '1' when (h_count = 799) else '0';
 v_tc <= '1' when (v_count = 520) else '0';
 
